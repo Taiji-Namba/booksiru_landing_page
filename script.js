e3b135120367aa1612205b65eaf817ff24d23d4e
Taiji Namba <namba9499@gmail.com>
2021-02-16 22:15:34 +0900

    HTML構造バグ修正

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-$(function () {
-  $('#signup-show').click(function(){
+$(function() {
+  $('.js-signup-show').click(function(){
     $('#signup-modal').fadeIn();
-  })
+  });
 
   $('#login-show').click(function(){
     $('#login-modal').fadeIn();
-  })
+  });
 
   $('.close-modal').click(function(){
     $('#login-modal').fadeOut();
     $('#signup-modal').fadeOut();
-  })
-})
+  });
+});
