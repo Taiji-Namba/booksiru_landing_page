c4368f5f22deba4b7ba75f455ea87e133b714065
Taiji Namba <namba9499@gmail.com>
2021-04-03 15:40:45 +0900

    スマホ・タブレットでのhover有効化

--- script.js
+++ script.js
@@ -1,48 +1,57 @@
 $(function() {
   // モーダル
-  $('.js-signup-show').click(function(){
-    $('#signup-modal').fadeIn();
-    $('html').addClass("modalset");
+  $(".js-signup-show").click(function () {
+    $("#signup-modal").fadeIn();
+    $("html").addClass("modalset");
   });
 
-  $('#login-show').click(function(){
-    $('#login-modal').fadeIn();
-    $('html').addClass("modalset");
+  $("#login-show").click(function () {
+    $("#login-modal").fadeIn();
+    $("html").addClass("modalset");
   });
 
-  $('.js-modal-close').click(function(){
-    $('#login-modal').fadeOut();
-    $('#signup-modal').fadeOut();
-    $('html').removeClass('modalset');
+  $(".js-modal-close").click(function () {
+    $("#login-modal").fadeOut();
+    $("#signup-modal").fadeOut();
+    $("html").removeClass("modalset");
   });
 
   // スライダーメニュー
-  let mySwiper = new Swiper('.swiper-container', {
+  let mySwiper = new Swiper(".swiper-container", {
     autoplay: {
       delay: 0,
     },
     loop: true,
     speed: 10000,
-    slidesPerView:1.8,
+    slidesPerView: 1.8,
     centeredSlides: true,
-    centeredSlidesBounds: true
+    centeredSlidesBounds: true,
   });
 
   // アコーディオンメニュー
-  $('.faq-list__item').on('click', function () {
-    let $answer = $(this).find('.faq-answer');
-    let $svg = $(this).find('img');
+  $(".faq-list__item").on("click", function () {
+    let $answer = $(this).find(".faq-answer");
+    let $svg = $(this).find("img");
 
-    if ($answer.hasClass('open')) {
-      $answer.removeClass('open');
+    if ($answer.hasClass("open")) {
+      $answer.removeClass("open");
       $answer.slideUp();
-      $svg.removeClass('svg-open');
-      $svg.addClass('svg-closed');
+      $svg.removeClass("svg-open");
+      $svg.addClass("svg-closed");
     } else {
-      $answer.addClass('open');
+      $answer.addClass("open");
       $answer.slideDown();
-      $svg.removeClass('svg-closed');
-      $svg.addClass('svg-open');
+      $svg.removeClass("svg-closed");
+      $svg.addClass("svg-open");
     }
   });
+
+  //スマホ・タブレットでのhover有効化
+  $("#diagnosis_btn")
+    .bind("touchstart", function () {
+      $(this).addClass("hover");
+    })
+    .bind("touchend", function () {
+      $(this).removeClass("hover");
+    });
 });
